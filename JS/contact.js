a78353fe6e3a0b5c3841820b0a14e7d355f4ad61
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2023-05-26 15:24:02 +0400

    Add files via upload

---
+++ JS/contact.js
@@ -0,0 +1,24 @@
+function initScrollUpButton() {
+	window.onscroll = function () {
+	   scrollFunction()
+	};
+ 
+	function scrollFunction() {
+	   var scrollup = document.getElementById("scrollup");
+	   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		  scrollup.classList.add("show");
+	   } else {
+		  scrollup.classList.remove("show");
+	   }
+	}
+ 
+	function scrollToTop() {
+	   document.body.scrollTop = 0;
+	   document.documentElement.scrollTop = 0;
+	}
+ 
+	const scrollupButton = document.getElementById("scrollup");
+	scrollupButton.addEventListener("click", scrollToTop);
+}
+
+ initScrollUpButton();
